a0de045a26df91c45f313e70d9e0d1f17a480ad0
honey_maya <sasaki123.321ikasas@gmail.com>
2022-10-10 21:05:49 +0900

    sns share text

---
+++ component/etc/sns.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/sns.css" type="text/css" />
+<ul class="sns_wrapper">
+    <li>
+        <div class="sns_common sns_1">
+            <a href="">
+                <img src="<?php echo get_template_directory_uri() ?>/image/SNS/twitter.png" alt="">
+            </a>
+        </div>
+    </li>
+    <li>
+        <div class="sns_common sns_2">
+            <a href="https://lin.ee/AhPBc0P">
+                <img src="<?php echo get_template_directory_uri() ?>/image/SNS/line.png" alt="">
+            </a>
+        </div>
+    </li>
+    <li>       
+        <div class="sns_common sns_3">
+            <a href="">
+                <img src="<?php echo get_template_directory_uri() ?>/image/SNS/facebook.png" alt="">
+            </a>
+        </div>
+    </li>
+    <li>       
+        <div class="sns_common sns_4">
+            <a href="">
+                <img src="<?php echo get_template_directory_uri() ?>/image/SNS/mail.png" alt="">
+            </a>
+        </div>
+    </li>
+</ul>
--- component/home/profile.php
+++ component/home/profile.php
@@ -1,16 +1,20 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/profile.css" type="text/css" />
 <section id="profile">
     <div class="p-content">
       <ul class="p-c-frame">
         <li class="p-c-f-li_left">
           <h3>及川和明</h3>
-          <p>1982年生まれ。立命館大学経済学部を卒業後、大手塾講師として就職。2012年より同学習塾の塾長兼運営者に着任。教え子の「やりたいことが見つからない」の一言をきっかけに、岩手の"おもしろいコト"探しに目覚める。</p>
+          <p>
+            立命館大学卒業後、大手塾講師として10年以上のキャリアを積み、延べ1,000人以上の生徒に受験指導を行ってきたプロ講師。
+            現在は保護者様向けの講演や、場所を選ばないオンライン塾「てのひら」の経営を手掛ける。
+          </p>
           <br>
-          <p>県内の「<span>すごい・面白い・ステキ</span>」な事業や活動発掘を通して、若者の「<span>ワクワク</span>」をかきたてられるメディアを目指しています。</p>
-          <br />
-          <p>協力してくださる企業・活動家さんを、只今 絶賛募集中！</p>
+          <p>教え子の「やりたいことが見つからない」の一言をきっかけに、岩手の"おもしろコト"探しに目覚める。</p>
+          <br>
+          <p>Ablestは、県内の「<span>すごい・面白い・ステキ</span>」な事業や活動を発掘し、若者の「<span>ワクワク</span>」をかきたてられるメディアを目指しています。協力してくださる企業・活動家さんを、只今 絶賛募集中！</p>
           <div class="p-c-f-lil-contact">
             <a href="#">
-              <p>ご相談はこちら</p>
+              <p>お問い合わせ</p>
             </a>
           </div>
           <div class="p-c-f-lil-profile">
@@ -22,6 +26,9 @@
         <li class="p-c-f-li_right">
           <div class="p-c-f-lir-frame">
 
+          </div>
+          <div class="p-c-f-lir-sns">
+            <?php get_template_part('component/etc/sns'); ?>
           </div>
         </li>
       </ul>
